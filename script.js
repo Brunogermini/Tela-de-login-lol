2e8ac110835fe3736fd406d29a0059758804e427
Bruno Henrique Germini <bgermini@uolinc.com>
2022-10-18 13:48:08 -0300

    Tela do LOL

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login_button');
+
+const handleFocus = ({target}) =>{
+   const span = target.previousElementSibling;
+   span.classList.add('span-active');
+}
+const handleFocusOut = ({target}) =>{
+
+    if (target.value ==''){
+        const span = target.previousElementSibling;
+    span.classList.remove('span-active'); 
+    }
+   
+}
+
+const handleChange = ()=> {
+const[username, password] = inputs;
+
+if (username.value && password.value.length >=8) {
+
+    button.removeAttribute('disabled');
+}
+else{
+
+    button.setAttribute('disabled','');
+}
+
+   
+}
+
+inputs. forEach((inputs) => inputs.addEventListener('focus', handleFocus));
+inputs. forEach((inputs) => inputs.addEventListener('focusout', handleFocusOut));
+inputs. forEach((inputs) => inputs.addEventListener('input', handleChange));
